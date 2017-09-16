99fbece38f47531ca770d2b9997ed849e44b2fd5
Gabriel Gene <gabrielgene97@gmail.com>
2017-09-16 19:03:17 -0300

    implement subject screen

---
+++ src/components/SubjectName/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SubjectName = () => (
+  <div className="subjectName">
+    <div className="imageFileSection">
+      <img src="https://files.slack.com/files-pri/T758QBX47-F758YTL5D/disciplina.png?pub_secret=8261e9e190" alt="disciplina" />
+    </div>
+    <h1>Disciplica (ENG001)</h1>
+    <h1>CÁLCULO I</h1>
+  </div>
+)
+
+export default SubjectName;
