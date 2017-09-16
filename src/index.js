530e486edd812f42361ce2b46b412f094f016546
Gabriel Gene <gabrielgene97@gmail.com>
2017-09-16 12:09:09 -0300

    VAI DESGRAÇAAAAAAAAAAA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { browserHistory, Route, Router, Redirect } from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker';
+import File from './pages/File';
+import Home from './pages/Home';
+import './index.css';
 
-ReactDOM.render(<h1>OK</h1>, document.getElementById('root'));
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <Router history={browserHistory}>
+      <Route path="/" component={Home} />
+      <Route path="/file" component={File} />
+      <Redirect path="**" to="/" />
+    </Router>
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/pages/File/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const File = () => (
+  <h1>File</h1>
+);
+
+export default File;
