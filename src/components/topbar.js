0b17f363702526882ec50c53ffcd5e54e167a2c3
Eric Murta <ericmurta@hotmail.com>
2017-09-16 17:45:21 -0300

    Added hamb menu to topbar

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl } from 'react-bootstrap'
+import { FormGroup, FormControl } from 'react-bootstrap';
+import { bubble as Menu } from 'react-burger-menu';
 
+const styles = {
+  bmBurgerButton: {
+    position: 'absolute',
+    width: '30px',
+    height: '20px',
+    top: '3%'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
 
 class Topbar extends Component {
   render() {
       return (
           <div className="Header">
+            <Menu styles={styles} width={'200px'} /> 
             <div className="Logo">
                 <img
                     className="Logo-img"
