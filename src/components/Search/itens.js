e3f42d8ed7a5b73f891e3172e10b42e075bb36c5
ravi <rafael.vitor@jusbrasil.com.br>
2017-09-17 02:43:21 -0300

    Add more fake data

--- src/components/Search/itens.js
+++ src/components/Search/itens.js
@@ -1,4 +1,5 @@
 export default [
+  // Calculo I
   { type: "Avaliação", subject: "Cálculo I", subtitle: "Derivadas e funções p...", likes: "35", time: "Há 2 semanas", lenght: "1,2 mb" },
   { type: "Apostila", subject: "Cálculo I", subtitle: "Todos assuntos da u...", likes: "800", time: "Há 4 meses", lenght: "6 gb" },
   { type: "Atividade", subject: "Cálculo I", subtitle: "Esbolçar gráficos", likes: "86", time: "Há 3 dias", lenght: "5,6 mb" },
@@ -13,4 +14,16 @@ export default [
   { type: "Atividade", subject: "Cálculo I", subtitle: "Esbolçar gráficos", likes: "86", time: "Há 3 dias", lenght: "5,6 mb" },
   { type: "Apostila", subject: "Cálculo I", subtitle: "Todos assuntos da u...", likes: "800", time: "Há 4 meses", lenght: "6 gb" },
   { type: "Apostila", subject: "Cálculo I", subtitle: "Todos assuntos da u...", likes: "800", time: "Há 4 meses", lenght: "6 gb" },
+  { type: "Discussão", subject: "Cálculo I", subtitle: "Duvida sobre Derivad...", likes: "35", time: "Há 2 semanas",  },
+  { type: "Discussão", subject: "Cálculo I", subtitle: "Pergunta: como faz...", likes: "800", time: "Há 4 meses",  },
+  { type: "Discussão", subject: "Cálculo I", subtitle: "Vou perder em calc..", likes: "86", time: "Há 3 dias",  },
+  
+  // Banco de dados
+  { type: "Avaliação", subject: "Banco de dados", subtitle: "Fazer queries em Sql..", likes: "35", time: "Há 2 semanas", lenght: "1,2 mb" },
+  { type: "Avaliação", subject: "Banco de dados", subtitle: "Bancos não relaciona...", likes: "35", time: "Há 2 semanas", lenght: "1,2 mb" },
+  { type: "Atividade", subject: "Banco de dados", subtitle: "Esbolçar tabelas", likes: "86", time: "Há 3 dias", lenght: "5,6 mb" },
+  { type: "Apostila", subject: "Banco de dados", subtitle: "Todos assuntos da u...", likes: "800", time: "Há 4 meses", lenght: "6 gb" },
+  { type: "Apostila", subject: "Banco de dados", subtitle: "Todos assuntos da u...", likes: "800", time: "Há 4 meses", lenght: "6 gb" },
+  { type: "Discussão", subject: "Banco de dados", subtitle: "Facil demais banco..", likes: "800", time: "Há 4 horas" },
+  { type: "Discussão", subject: "Banco de dados", subtitle: "Difícil demais, desisto.", likes: "800", time: "Há 4 meses" },
 ]
