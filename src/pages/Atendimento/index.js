b5b48aa6e29e32439f7f733cc366f12fa3815b0a
Gabriel Gene <gabrielgene97@gmail.com>
2017-09-17 08:36:40 -0300

    Implement atendimento and fix bard

--- src/pages/Atendimento/index.js
+++ src/pages/Atendimento/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Topbar from '../../components/Topbar';
+import './style.css';
 
 const Atendimento = () => (
   <div>
-    <iframe style={{ border: 'white', width: '100%', height: 736 }} src="http:\\localhost:3030" />
-  </div>
+    <div className="atTopBar">
+      <Topbar showSearch={true} />
+    </div>
+    <iframe style={{ border: 'white', width: '100%', height: 617}} src="http:\\localhost:3030" />
+  </div >
 )
 
 export default Atendimento;
